2a7cbde6eb0bba7f80459d7af3bbc6493ee5a97c
wuxuweilalala <2509165479@qq.com>
2020-08-17 20:45:56 +0800

    [id].tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import styles from '../styles/Home.module.scss';
 import Link from 'next/link';
 import jpg from '../assets/1.jpg';
 import {GetServerSideProps, NextPage} from 'next';
-import {UAParser} from 'ua-parser-js';
 import {getDatabaseConnection} from '../libs/getDatabaseConnection';
 import {Post} from '../src/entity/Post';
 import {User} from '../src/entity/User';
@@ -11,15 +10,18 @@ import {Comment} from '../src/entity/Comment';
 import {useEffect, useState} from 'react';
 
 type Props = {
-    posts:Post[],
+    posts: Post[],
 }
 
 const Home: NextPage<Props> = (props) => {
     const {posts} = props;
     return (
-        <div className={styles.container}>
-            {posts.map((post)=>(
-                <div key={post.id}>{post.title}</div>
+        <div>
+            <h1>文章列表</h1>
+            {posts.map((post) => (
+                <Link key={post.id} href={`/posts/${post.id}`}>
+                    <a >{post.title}</a>
+                </Link>
             ))}
         </div>
     );
@@ -30,9 +32,9 @@ export default Home;
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const connection = await getDatabaseConnection();
     console.log('connect');
-    const posts =await connection.manager.find(Post)
-    const users =await connection.manager.find(User)
-    const comments =await connection.manager.find(Comment)
+    const posts = await connection.manager.find(Post);
+    const users = await connection.manager.find(User);
+    const comments = await connection.manager.find(Comment);
     return {
         props: {
             posts: JSON.parse(JSON.stringify(posts))
