699bc61e2dff405bb3cc9f61a7963f307a219d33
wuxuweilalala <2509165479@qq.com>
2020-08-19 21:26:49 +0800

    sign_up checkout and success sign_up

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -1,20 +1,48 @@
 import {NextApiHandler} from 'next';
 import {getDatabaseConnection} from '../../../libs/getDatabaseConnection';
 import {User} from '../../../src/entity/User';
-const PostsIndex: NextApiHandler = async (req, res) => {
-    const {username,password,passwordConfirm} = req.body
-    res.statusCode = 200;
-    const connection= await getDatabaseConnection();
-    const user = new User();
-    user.username = username;
-    if(password !== passwordConfirm){
-        const errors = {passwordConfirm:['密码不匹配']}
-        res.statusCode = 422
-        res.setHeader('Content-Type','application/json')
-        res.json(JSON.stringify(errors))
-        res.end()
-    }
+import md5 from 'md5';
 
-}
+const PostsIndex: NextApiHandler = async (req, res) => {
+    const {username, password, passwordConfirm} = req.body;
+    res.statusCode = 200;
+
+    const errors = {
+        username: [] as string[], password: [] as string[], passwordConfirm: [] as string[]
+    };
+    if (username.trim() === '') {
+        errors.username.push('不能为空');
+    }
+    if (!/[a-zA-Z0-9]/.test(username.trim())) {
+        errors.username.push('格式不合法');
+    }
+    if (username.trim().length > 42) {
+        errors.username.push('太长');
+    }
+    if (username.trim().length <= 3) {
+        errors.username.push('太短');
+    }
+    if (password === '') {
+        errors.password.push('不能为空');
+    }
+    if (password !== passwordConfirm) {
+        errors.passwordConfirm.push('密码不匹配');
+    }
+    const hasErrors = Object.values(errors).find(v => v.length > 0);
+    res.setHeader('Content-Type', 'application/json;charset=utf-8');
+    if (hasErrors) {
+        res.statusCode = 422;
+        res.write(JSON.stringify(errors));
+    } else {
+        const connection = await getDatabaseConnection();
+        const user = new User();
+        user.username = username;
+        user.passwordDigest = md5(password);
+        await connection.manager.save(user);
+        res.statusCode = 200;
+        res.write(JSON.stringify(user));
+    }
+    res.end();
+};
 
 export default PostsIndex;
