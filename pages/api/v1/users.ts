47706363d98b3cf8f78882f1027614b04eae4f85
wuxuweilalala <2509165479@qq.com>
2020-08-21 21:44:46 +0800

    User validate hasErrors

--- pages/api/v1/users.ts
+++ pages/api/v1/users.ts
@@ -5,39 +5,23 @@ import md5 from 'md5';
 
 const PostsIndex: NextApiHandler = async (req, res) => {
     const {username, password, passwordConfirm} = req.body;
-    res.statusCode = 200;
-
-    const errors = {
-        username: [] as string[], password: [] as string[], passwordConfirm: [] as string[]
-    };
-    if (username.trim() === '') {
-        errors.username.push('不能为空');
-    }
-    if (!/[a-zA-Z0-9]/.test(username.trim())) {
-        errors.username.push('格式不合法');
-    }
-    if (username.trim().length > 42) {
-        errors.username.push('太长');
-    }
-    if (username.trim().length <= 3) {
-        errors.username.push('太短');
-    }
-    if (password === '') {
-        errors.password.push('不能为空');
-    }
-    if (password !== passwordConfirm) {
-        errors.passwordConfirm.push('密码不匹配');
-    }
-    const hasErrors = Object.values(errors).find(v => v.length > 0);
+    const connection = await getDatabaseConnection();
     res.setHeader('Content-Type', 'application/json;charset=utf-8');
-    if (hasErrors) {
+
+
+    const user = new User();
+    user.username = username.trim();
+    user.password = password;
+    user.passwordConfirm = passwordConfirm;
+    user.passwordDigest = md5(password);
+
+    await user.validate();
+
+
+    if (user.hasErrors()) {
         res.statusCode = 422;
-        res.write(JSON.stringify(errors));
+        res.write(JSON.stringify(user.errors));
     } else {
-        const connection = await getDatabaseConnection();
-        const user = new User();
-        user.username = username;
-        user.passwordDigest = md5(password);
         await connection.manager.save(user);
         res.statusCode = 200;
         res.write(JSON.stringify(user));
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -9,21 +9,55 @@ import {
 } from 'typeorm';
 import {Post} from './Post';
 import {Comment} from './Comment';
+import {getDatabaseConnection} from '../../libs/getDatabaseConnection';
 
 @Entity('users')
 export class User {
     @PrimaryGeneratedColumn('increment')
-    id:number;
+    id: number;
     @Column('varchar')
-    username:string;
+    username: string;
     @Column('varchar')
-    passwordDigest:string;
+    passwordDigest: string;
     @CreateDateColumn()
-    createdAt:Date;
+    createdAt: Date;
     @UpdateDateColumn()
-    updatedAt:Date;
-    @OneToMany(type => Post,post=>post.author)
-    posts:Post[];
-    @OneToMany(type => Comment,comment=>comment.user)
-    comments:Comment[]
+    updatedAt: Date;
+    @OneToMany(type => Post, post => post.author)
+    posts: Post[];
+    @OneToMany(type => Comment, comment => comment.user)
+    comments: Comment[];
+
+    errors = {username: [] as string[], password: [] as string[], passwordConfirm: [] as string[]};
+    password: string;
+    passwordConfirm: string;
+
+    async validate() {
+        const found =await (await getDatabaseConnection()).manager.find(User, {username: this.username});
+
+        if (found.length > 0) {
+            this.errors.username.push('用户名已存在,不能重复注册');
+        }
+        if (this.username.trim() === '') {
+            this.errors.username.push('不能为空');
+        }
+        if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+            this.errors.username.push('格式不合法');
+        }
+        if (this.username.trim().length > 42) {
+            this.errors.username.push('太长');
+        }
+        if (this.username.trim().length <= 3) {
+            this.errors.username.push('太短');
+        }
+        if (this.password === '') {
+            this.errors.password.push('不能为空');
+        }
+        if (this.password !== this.passwordConfirm) {
+            this.errors.passwordConfirm.push('密码不匹配');
+        }
+    }
+    hasErrors(){
+        return  !!Object.values(this.errors).find(v => v.length > 0);
+    }
 }
