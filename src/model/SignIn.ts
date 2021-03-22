b352ad3146da42b4547258815ad2c3ebb64b4b5a
403 <wuxuwei@codemao.cn>
2021-03-22 17:40:16 +0800

    test2

--- src/model/SignIn.ts
+++ src/model/SignIn.ts
@@ -18,7 +18,7 @@ export class SignIn {
         this.user = user;
         if (user) {
             if (user.passwordDigest !== md5(this.password)) {
-                this.errors.password.push('密码与用户名不匹配');
+                 this.errors.password.push('密码与用户名不匹配');
             }
         } else {
             this.errors.username.push('用户名不存在');
